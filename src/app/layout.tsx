e8a0bda89f8ebf0f459cb5d7cfff893a6bbc55a3
Thomas Zachary <tomzachary@live.com>
2025-07-01 20:01:35 -0500

    adjusting a few recommendations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
-        {children}
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
